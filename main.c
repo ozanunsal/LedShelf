bb010891f637bcd028d52676cb310ebe2039e4cf
ozanunsal <ozanunsal@gmail.com>
2017-06-09 23:32:51 +0200

    USART with IRQ, GPIO bring up
    Change-Id: I92c95f0135d09848816bbe2b7b9861e4455382a3

--- main.c
+++ main.c
@@ -1,45 +1,112 @@
-#include "stm32f4xx.h"                  // Device header
-#include "stm32f4xx_hal.h"              // Keil::Device:STM32Cube HAL:Common
+#include "stm32f4xx.h"// Device header
 
-// Global Timing Delay variable
-uint32_t TimingDelay;
-
+volatile uint32_t msTicks;
+volatile char RxBuf[];
 void SysTick_Handler(void)
 {
-	if (TimingDelay != 0) TimingDelay--;
+	msTicks++;
 }
 
-void delay(uint32_t time)
+void delay(__IO uint32_t dValue)
 {
-	TimingDelay = time;
-	while(TimingDelay != 0);
+	uint32_t curTick = msTicks;
+	while ((msTicks - curTick) < dValue);
+}
+
+void USART_puts(USART_TypeDef *USARTx, volatile char *str)
+{
+	while(*str){
+		//while(!(USARTx->SR & 0x040)); // Wait for the sending finish
+		while(USART_GetFlagStatus(USARTx, USART_FLAG_TC) == RESET);
+		USART_SendData(USARTx, *str);
+		str++;
+	}
+}
+
+//USART Interrupt request handler
+void USART2_IRQHandler(void)
+{
+		if(USART_GetITStatus(USART2, USART_IT_RXNE)){
+			static int cnt = 0;
+			//flag is set
+			char c = USART2->DR; // Transfer the byte in data register
+			if (c != '\n') {
+				RxBuf[cnt] = c;
+				cnt++;
+			} else {
+				RxBuf[cnt] = '\0';
+				cnt = 0;
+				USART_puts(USART2, RxBuf);
+			}
+		}
 }
 
 int main()
 {
-	// Initialize GPIO Structure
-		GPIO_InitTypeDef GPIO_InitStructure;
-	//Enable the GPIO Clock for port D
-	__GPIOD_CLK_ENABLE();
-
-	//Configures the GPIO Structure
-	GPIO_InitStructure.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-	GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStructure.Pull = GPIO_PULLDOWN;
-	GPIO_InitStructure.Speed = GPIO_SPEED_LOW;
-
-	//Initialize the GPIO
-	HAL_GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-	//Initialize Timer
-	SysTick_Config(SystemCoreClock / 1000); // Configure Systick interrupts as 1ms
-
+	// Initialize GPIO
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+		
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	
+	//Configure SysTick
+	if (SysTick_Config( SystemCoreClock / 1000)) { // 1ms SysTick
+		/*ToDO Give Error Indicator*/
+	}
+	
+	//Initialize USART
+	USART_InitTypeDef USART_InitStructure;
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE); // enable USART clock
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); // enable gpio A ports
+	//Setup gpio pins Tx(PA2) and Rx(PA3)
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+	
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART2, &USART_InitStructure);
+	
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE); // Declare USART generate interrupts for Rx only
+	
+	//Initialize NVIC we want use USART in interrupt mode, so we need NVIC
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; //highest priorty for peripheral
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	USART_Cmd(USART2, ENABLE); // Enable USART2 Peripheral
+	
 	while(1)
 	{
-		delay(300); // This generates 300ms
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-		delay(300);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+		USART_puts(USART2, "Hello World\n");
+
+		GPIO_SetBits(GPIOD, GPIO_Pin_12);
+		GPIO_SetBits(GPIOD, GPIO_Pin_13);
+		GPIO_SetBits(GPIOD, GPIO_Pin_14);
+		GPIO_SetBits(GPIOD, GPIO_Pin_15);
+		delay(200);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_12);	
+		GPIO_ResetBits(GPIOD, GPIO_Pin_13);	
+		GPIO_ResetBits(GPIOD, GPIO_Pin_14);	
+		GPIO_ResetBits(GPIOD, GPIO_Pin_15);	
+		delay(200);
 	}
-	return 0;
 }
